3e8461eb0935e0229163643cc5ebc2f05c9f9697
Mogamat-Muneeb <davimuneeb785@gmail.com>
2022-01-19 17:52:13 +0200

    still busy

--- main.js
+++ main.js
@@ -17,3 +17,12 @@
 //   document.body.scrollTop = 0; 
 //   document.documentElement.scrollTop = 0;
 // }
+
+const menu_btn = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+menu_btn.addEventListener('click', () => {
+
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+});
